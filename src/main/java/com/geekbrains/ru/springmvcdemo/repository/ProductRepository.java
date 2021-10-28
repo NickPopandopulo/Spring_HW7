ce9f21db13fe89b0f9eb723d0195f449df1e87bd
Nikita Popandopulo <nikas.popandopulos@gmail.com>
2021-10-28 20:37:10 +0700

    Homework 7

--- src/main/java/com/geekbrains/ru/springmvcdemo/repository/ProductRepository.java
+++ src/main/java/com/geekbrains/ru/springmvcdemo/repository/ProductRepository.java
@@ -2,9 +2,10 @@ package com.geekbrains.ru.springmvcdemo.repository;
 
 import com.geekbrains.ru.springmvcdemo.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
 }
